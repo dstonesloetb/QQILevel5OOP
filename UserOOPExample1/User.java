8f188a4b8ed419e02c46e148ecde76645a29f915
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2025-01-07 14:59:48 +0000

    Create User.java

---
+++ UserOOPExample1/User.java
@@ -0,0 +1,14 @@
+package com.mycompany.oopuserexample;
+
+//Template or blueprint to create user objects
+
+//Class declaration
+public class User {
+    //Decribe the attributes or properties
+    //of the object 
+    //instance variables 
+    public String name;
+    public String membership;
+    public int age;
+    
+} //end class 
