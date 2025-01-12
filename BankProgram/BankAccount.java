ec89fb89b7a1bbfe523e10617c787dbf32d732d2
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2025-01-12 18:48:12 +0000

    Create BankAccount.java

---
+++ BankProgram/BankAccount.java
@@ -0,0 +1,35 @@
+package com.mycompany.bankprogram;
+
+/**
+ * Program:  BankAccount Class
+ * @author Student
+ * Date: 
+ */
+
+public class BankAccount 
+{
+    //instance variable
+   // ** Insert relevant code here **
+    private static final double MIN_BALANCE = 0.0;
+
+    //Contructor to set balance to zero
+    // ** Insert relevant code here **
+
+    //Method to allow user deposit money
+    //and display deposited amount
+    public void deposit(double amount) {
+         // ** Insert relevant code here **
+        
+    }
+
+    //Method to withdraw money if the 
+    //balance minus the amount is not less than the minimum balance 
+    
+    public void withdraw(double amount) {
+          // ** Insert relevant code here **
+    }
+
+    //Method to output the current balance 
+    // ** Insert relevant code here **
+   
+} //end class 
