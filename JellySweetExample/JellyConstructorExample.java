335a6e79a93d4d5318327ccddfab95ab706f9d3e
dstonesloetb <145108402+dstonesloetb@users.noreply.github.com>
2025-01-14 22:21:15 +0000

    Create JellyConstructorExample.java

---
+++ JellySweetExample/JellyConstructorExample.java
@@ -0,0 +1,14 @@
+package com.mycompany.JellySweetExample;
+
+public class JellyConstructorExample {
+
+    public static void main(String[] args) {
+        //Create a Jelly object using the five argument constructor
+        Jelly j1 = new Jelly("Haribo", 1.2f, Flavour.MINT, 300f, "31/12/2025");
+        
+        //Display the first jelly using the toString method
+        System.out.println(j1.toString());
+                  
+        
+    } //end main
+} //end class 
